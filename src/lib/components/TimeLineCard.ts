946edadd1cd679ffdd5c2d6e54b7062dd221df4a
Waren Gador <waren.gador15@gmail.com>
2025-08-18 01:33:33 +0800

    update

--- src/lib/components/TimeLineCard.ts
+++ src/lib/components/TimeLineCard.ts
@@ -7,19 +7,21 @@ import { useUniqueId } from '../hooks'
 
 
 export default function TimeLineCard() {
+    return mapper(
+        timeline.map(({ year, bg, desc, transition }) => {
+            const { dataId } = useUniqueId();
 
-    const { dataId } = useUniqueId()
-
-    const ui = mapper(timeline.map(({ year, bg, desc, transition }) => html`
-        <div data-id="${dataId}" class="mb-8 relative animate__animated ${transition}">
-            <div
-                class="absolute -left-[26px] top-1 w-5 h-5 ${bg} rounded-full border-4 border-white animate__animated animate__bounceIn animate__delay-5s">
-            </div>
-            <p class="text-2xl font-semibold">${year}</p>
-            ${mapper(desc.map((item) => html`<p>${item}</p>`))}
-        </div>
-    `))
-
-    return ui
-
+            return html`
+                <div data-id="${dataId}" class="mb-8 relative animate__animated ${transition}">
+                    <div
+                        class="absolute -left-[26px] top-1 w-5 h-5 ${bg} rounded-full border-4 border-white animate__animated animate__bounceIn animate__delay-5s">
+                    </div>
+                    <p class="text-2xl font-semibold">${year}</p>
+                    ${
+                        mapper(desc.map((item) => html`<p>${item}</p>`))
+                    }
+                </div>
+            `;
+        })
+    );
 }
